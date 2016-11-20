e3d51b541242f865f6df1f7e2d85eae9059fa14f
Luis Rivera Rivera <luisriverarivera@macintosh-2.local>
2016-11-20 13:11:34 -0400

    Revision

--- UPRM GUIDE 1.0/ViewController.swift
+++ UPRM GUIDE 1.0/ViewController.swift
@@ -10,10 +10,10 @@ import UIKit
 import MapKit
 import CoreLocation
 
-class ViewController: UIViewController, MKMapViewDelegate {
+class ViewController: UIViewController{
 
-    @IBOutlet weak var mapView: MKMapView!
     
+    @IBOutlet weak var map: MKMapView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -21,21 +21,6 @@ class ViewController: UIViewController, MKMapViewDelegate {
         
         // Set Initial location to Mayaguez, Puerto Rico, UPRM 18.211335, -67.139150
         
-        var latitude: CLLocationDegrees = 18.21
-        
-        var longitude: CLLocationDegrees = -67.13
-
-        var latitudeDelta: CLLocationDistance = 0.05
-        
-        var longitudeDelta: CLLocationDegrees = 0.05
-        
-        var span: MKCoordinateSpan = MKCoordinateSpan(latitudeDelta: CLLocationDegrees, longitudeDelta: CLLocationDegrees)
-        
-        var location: CLLocationCoordinate2D = CLLocationCoordinate2D(latitude: CLLocationDegrees, longitude: CLLocationDegrees)
-        
-        var region: MKCoordinateRegion = MKCoordinateRegion(center: CLLocationCoordinate2D, span: span )
-        
-        mapView.setRegion(region,animated:true)
     }
 
     override func didReceiveMemoryWarning() {
