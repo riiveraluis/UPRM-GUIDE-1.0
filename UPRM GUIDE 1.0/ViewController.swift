95468c035a1177453d4bc0cd41c18eaa5816c3d3
Luis Rivera Rivera <luisriverarivera@luis-imac.local>
2016-11-27 01:26:03 -0400

    Revision

--- UPRM GUIDE 1.0/ViewController.swift
+++ UPRM GUIDE 1.0/ViewController.swift
@@ -10,14 +10,28 @@ import UIKit
 import MapKit
 import CoreLocation
 
-class ViewController: UIViewController{
+class ViewController: UIViewController, MKMapViewDelegate{
 
     
-    @IBOutlet weak var map: MKMapView!
-    
+    @IBOutlet var map: MKMapView!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        let latitude: CLLocationDegrees = 27.1
+        
+        let longitude: CLLocationDegrees = 78.0
+        
+        let lanDelta: CLLocationDegrees = 0.05
+        
+        let lonDelta: CLLocationDegrees = 0.05
+        
+        let span = MKCoordinateSpan(latitudeDelta: lanDelta, longitudeDelta: lonDelta)
+        
+        let coordinates = CLLocationCoordinate2D(latitude: latitude, longitude: longitude)
+        
+        let region = MKCoordinateRegion(center: coordinates, span: span)
+        
+        map.setRegion(region, animated: true)
         
         // Set Initial location to Mayaguez, Puerto Rico, UPRM 18.211335, -67.139150
         
